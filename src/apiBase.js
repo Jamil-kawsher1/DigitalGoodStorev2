4a3018fdc86a56626a40b3eed9db9363f7af05ad
Jamil-kawsher1 <jamilkawsher@gmail.com>
2025-10-01 09:45:43 +0600

    Base url updated

--- src/apiBase.js
+++ src/apiBase.js
@@ -1,7 +1,4 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-
-export const API_BASE = "http://localhost:4002";
+export const API_BASE = import.meta.env.VITE_API_BASE_URL || "https://easybuybd.space/";
 const TOKEN_KEY = "app_token";
 const USER_DATA_KEY = "user_data";
 
